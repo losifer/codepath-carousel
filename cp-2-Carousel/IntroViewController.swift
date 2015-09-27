462eb905680fa5a28591f05df10a1478b38ddac8
losifer <carloscanash@gmail.com>
2015-09-27 15:27:34 -0700

    Updates.
    Cleaning some code

--- cp-2-Carousel/IntroViewController.swift
+++ cp-2-Carousel/IntroViewController.swift
@@ -19,10 +19,7 @@ class IntroViewController: UIViewController , UIScrollViewDelegate {
     @IBOutlet weak var photoImage05: UIImageView!
     @IBOutlet weak var photoImage06: UIImageView!
     
-    var yOffsets : [Float] = [-285, -240, -415, -408, -480, -500]
-    var xOffsets : [Float] = [-30, 75, -66, 10, -200, -15]
-    var scales : [Float] = [1, 1.65, 1.7, 1.6, 1.65, 1.65]
-    var rotations : [Float] = [-10, -10, 10, 10, 10, -10]
+   
     
  
     
@@ -57,12 +54,9 @@ class IntroViewController: UIViewController , UIScrollViewDelegate {
     func scrollViewDidScroll(scrollView: UIScrollView) {
         let offset = CGFloat(scrollView.contentOffset.y)
         
-        // content offset: 0 -> 568
-        // x offset: -30 -> 0
-        // y offset: -285 -> 0
+    
         
-        var tx = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -30, r2Max: 0)
-        var ty = convertValue(offset, r1Min: 0, r1Max: 568, r2Min: -285, r2Max: 0)
+      
         
         
         photoImage01.transform = CGAffineTransformMakeTranslation(CGFloat(tx), CGFloat(ty))
